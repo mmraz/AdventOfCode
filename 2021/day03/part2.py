fbb4f7fa94eb9370dc76fa3da80fcdc10a9e6a67
Michael Mraz <github@mraz.org>
2021-12-03 10:39:33 -0600

    2021 day 3 part 2

---
+++ 2021/day03/part2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+DEBUG = False
+
+with open('input', 'r') as f:
+    inputs = f.read().splitlines()
+
+data = [int(entry, 2) for entry in inputs]
+
+bitw = len(inputs[0])
+if DEBUG:
+    print(f"bit width {bitw}")
+
+def parse(data, width, least=False):
+    # start at least significant bit and step back
+    for i in range(width-1,-1,-1):
+        count = 0
+        for f in data:
+            count += f >> i & 1
+        if least:
+            count = count < len(data)//2 + len(data)%2
+        else:
+            count = count >= len(data)//2 + len(data)%2
+        data = [x for x in data if x>>i & 1 == count]
+        if DEBUG:
+            print(f"data length {len(data)}")
+        if len(data) == 1:
+            return data.pop()
+
+# duplicate data list as it's filtered in place during parsing
+datac = data.copy()
+cotwo = parse(data, bitw, True)
+oxygen = parse(datac, bitw)
+print(oxygen * cotwo)
