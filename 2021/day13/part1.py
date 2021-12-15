f0436b28bfb88207ed85a64e98fc6eba10a944c5
Michael Mraz <michael@mraz.org>
2021-12-14 22:28:20 -0600

    2021 day 13

---
+++ 2021/day13/part1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import numpy as np
+
+DEBUG = False
+
+with open('input', 'r') as f:
+    inputs = f.read().splitlines()
+    folds = [tuple(f.split()[2].split('=')) for f in inputs if 'f' in f]
+    if DEBUG:
+        print(folds)
+
+points = np.genfromtxt('input', converters={0: lambda x: int(x), 1: lambda y: int(y)}, delimiter=',', comments='fold')
+if DEBUG:
+    print(points)
+max_x = max([x for (x, y) in points])+1
+max_y = max([y for (x, y) in points])+1
+if DEBUG:
+    print(f"max_x {max_x} max_y {max_y}")
+paper = np.zeros(shape=(max_y, max_x), dtype=bool)
+for (x, y) in points:
+    paper[y][x] = True
+if DEBUG:
+    print(paper)
+
+
+def foldit(paper, folds):
+    for axis, fold in folds:
+        fold = int(fold)
+        if DEBUG:
+            print(f"axis {axis} fold {fold}")
+        if axis == 'x':
+            left = paper[:, :fold]
+            right = paper[:, 2*fold:fold:-1]
+            paper = left | right
+        elif axis == 'y':
+            top = paper[:fold]
+            bottom = paper[2*fold:fold:-1, :]
+            paper = top | bottom
+        if DEBUG:
+            print(paper)
+        return(paper)
+
+
+paper = foldit(paper, folds)
+print(paper.sum())
